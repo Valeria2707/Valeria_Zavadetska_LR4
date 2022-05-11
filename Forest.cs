bdc02aec2931297ca2d13b873f4eef2501c09953
Валерія Завадецька <valeria.zavadetska@gmail.com>
2022-05-11 13:04:41 +0300

    lab4

---
+++ Forest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using laba4;
+
+namespace laba4
+{
+    class Forest
+    {
+        List<IForestElement> trees = new List<IForestElement>();
+
+
+        public void Plant(IForestElement tree)
+        {
+            trees.Add(tree);
+        }
+
+        public void CutDown(IForestElement tree)
+        {
+            trees.Remove(tree);
+        }
+
+        public int ElementsCount()
+        {
+            return trees.Count;
+        }
+    }
+}
